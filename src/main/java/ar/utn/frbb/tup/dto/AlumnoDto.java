b93b40a0809469896a03f389f5f01e5d284a4ba7
Marcia Alvarez <alvarezmarciabelen@gmail.com>
2024-02-24 20:41:37 -0300

    AlumnoDTO attributes

--- src/main/java/ar/utn/frbb/tup/dto/AlumnoDto.java
+++ src/main/java/ar/utn/frbb/tup/dto/AlumnoDto.java
@@ -1,5 +1,36 @@
 package ar.utn.frbb.tup.dto;
 
-public class AlumnoDto {
+import ar.utn.frbb.tup.model.Carrera;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
+public class AlumnoDto {
+    private Integer idAlumno;
+    private String nombre;
+    private String apellido;
+    private Integer dni;
+    private Carrera carrera;
+
+    @Override
+    public int hashCode() {
+        return this.idAlumno;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "AlumnoDto{" +
+                "idAlumno=" + idAlumno +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", dni=" + dni +
+                ", carrera=" + carrera +
+                '}';
+    }
 }
